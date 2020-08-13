88a6955886fb6be6aa8229fecff5939e8eb42e6b
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-13 22:25:44 +0200

    functionality done

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,6 +1,7 @@
 const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
-    let results = calculateMetricBMI(weighInput.value, heightInput.value)
-    let displayResultsElements = `<h1> Your BMI value is: ${results}</h1>´
+    let results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let displayResultsElements = document.getElementById('results')
+    displayResultsElements.innerHTML = `<h1> Your BMI value is: ${results} </h1>`
 }
