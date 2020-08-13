644fb95d6ab0b8d0d837589a79178aae5acf93fb
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-13 22:37:59 +0200

    working

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,6 +2,7 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let message = getBMIClassification(results)
     let displayResultsElements = document.getElementById('results')
-    displayResultsElements.innerHTML = `<h1> Your BMI value is: ${results} </h1>`
+    displayResultsElements.innerHTML = `<h1> Your BMI value is: ${results} and you are ${message} </h1>`
 }
